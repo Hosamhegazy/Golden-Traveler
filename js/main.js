cd978a57bf8f1bc6a613ff2a62556fce48bce91c
Hosamhegazy <hosam.a.hegazy@outlook.com>
2024-12-01 16:07:32 +0400

    Modified

--- js/main.js
+++ js/main.js
@@ -115,8 +115,8 @@
     });
 
     
-   // Back to top button
-   $(window).scroll(function () {
+// Back to top button
+$(window).scroll(function () {
     if ($(this).scrollTop() > 300) {
         $('.back-to-top').fadeIn('slow');
     } else {
@@ -124,9 +124,9 @@
     }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({scrollTop: 0}, 500, 'easeInOutExpo');
         return false;
-    }); 
+    });
 
 })(jQuery);
 
